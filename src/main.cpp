10995015b4279326f5240a71d07c5d6942635fb0
Guillaume Arseneault <gllm@artificiel.org>
2017-11-12 14:37:57 -0500

    ~version d opengl

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,21 @@
 
 //========================================================================
 int main( ){
-
-    ofGLESWindowSettings settings;
-    settings.width = 1280;
-    settings.height = 720;
-    settings.setGLESVersion(2);
-    ofCreateWindow(settings);
+    ofGLWindowSettings settings;
+    settings.setGLVersion(4,1); /// < select your GL Version here
+    ofCreateWindow(settings); ///< create your window here
     
-    ofRunApp( new ofApp());
-
+    ofRunApp(new ofApp());
     
-    //    ofGLWindowSettings settings;
-//    settings.setGLVersion(4,1); /// < select your GL Version here
-//    ofCreateWindow(settings); ///< create your window here
+    
+//    ofGLESWindowSettings settings;
+//    settings.width = 1280;
+//    settings.height = 720;
+//    settings.setGLESVersion(2);
+//    ofCreateWindow(settings);
 //    
-//    ofRunApp(new ofApp());
-    
+//    ofRunApp( new testApp());
+
     //	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
 //
 //	// this kicks off the running of my app
