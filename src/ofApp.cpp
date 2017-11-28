df8d200ded0f18417a0635c61db844661a7662a3
Guillaume Arseneault <gllm@artificiel.org>
2017-11-27 21:24:59 -0500

    ajout de click droit pour gui show

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -154,6 +154,8 @@ void ofApp::mouseDragged(int x, int y, int button){
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
+     if( button == 2 ){gui_draw=!gui_draw;}
+
 
 }
 
