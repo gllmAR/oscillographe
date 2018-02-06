4812cf699a10fa91b2df88c52db663ffb6af715e
Guillaume Arseneault <gllm@artificiel.org>
2018-02-06 13:58:14 -0500

    revert gl config

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,18 @@
 //========================================================================
 int main( ){
     
-#ifdef TARGET_RASPBERRY_PI
-    ofGLESWindowSettings settings;
-    settings.setGLESVersion(2);
-    ofCreateWindow(settings);
-#else
-    
+//#ifdef TARGET_RASPBERRY_PI
+//    ofGLESWindowSettings settings;
+//    settings.setGLESVersion(2);
+//    ofCreateWindow(settings);
+//#else
+//    
     ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
     
     // this kicks off the running of my app
     // can be OF_WINDOW or OF_FULLSCREEN
     // pass in width and height too:
     ofRunApp(new ofApp());
-#endif
+//#endif
 
 };
