c30392a773d233a516aea4b37e6151b1d85cb433
Guillaume Arseneault <gllm@artificiel.org>
2018-02-14 22:26:06 -0500

    testing custom renderer

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,25 @@
 #include "ofMain.h"
 #include "ofApp.h"
 
+
 //========================================================================
 int main( ){
     
 #ifdef TARGET_RASPBERRY_PI
-    ofGLESWindowSettings settings;
-    settings.setGLESVersion(2);
-    ofCreateWindow(settings);
+    #include "ofGLES2Renderer.h"
+//    ofGLESWindowSettings settings;
+//    settings.setGLESVersion(2);
+//    ofCreateWindow(settings);
+    
+    ofGLES2Renderer * renderer = new ofGLES2Renderer();
+    ofSetCurrentRenderer(ofPtr<ofBaseRenderer>(renderer)); // switch to GLES2 renderer.
+    
+    ofSetupOpenGL(1024,768, OF_WINDOW);// <-------- setup the GL context
+    
+    // this kicks off the running of my app
+    // can be OF_WINDOW or OF_FULLSCREEN
+    // pass in width and height too:
+    ofRunApp( new ofApp());
 #else
     
     ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
