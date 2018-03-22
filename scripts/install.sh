3f3a4926e6f52e0f6f2a0c40f2e3cc18106b3fe9
gllmar <gllm@artificiel.org>
2018-03-22 16:08:20 +0000

    tweak sensor and install script to updated sensor management

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
-# get the absoluth path
+## get the absoluth path
 pushd `dirname $0` > /dev/null
 SCRIPTPATH=`pwd -P`
 popd > /dev/null
 
 sudo systemctl stop oscillo.service
 
-sudo ln -s -f $SCRIPTPATH/oscillo.service /etc/systemd/system/oscillo.service
-sudo ln -s -f $SCRIPTPATH/sensor_1.service /etc/systemd/system/sensor_1.service
-sudo ln -s -f $SCRIPTPATH/sensor_2.service /etc/systemd/system/sensor_2.service
+sudo cp  $SCRIPTPATH/oscillo.service /etc/systemd/system/oscillo.service
+sudo cp  $SCRIPTPATH/sensor_1.service /etc/systemd/system/sensor_1.service
+sudo cp  $SCRIPTPATH/sensor_2.service /etc/systemd/system/sensor_2.service
 
 sudo systemctl daemon-reload
-sudo systemctl start oscillo.service
 sudo systemctl restart sensor_1.service
 sudo systemctl restart sensor_2.service
+sudo systemctl enable sensor_1 sensor_2
+sudo systemctl restart oscillo.service
 
-sudo cp $SCRIPTPATH/80-oscilloscope.sh /etc/X11/xinit/xinitrc.d/80-oscilloscope.sh
-sudo chmod +x /etc/X11/xinit/xinitrc.d/80-oscilloscope.sh
+## should be nested in a if statement (is xorg?)
+# sudo cp $SCRIPTPATH/80-oscilloscope.sh /etc/X11/xinit/xinitrc.d/80-oscilloscope.sh
+# sudo chmod +x /etc/X11/xinit/xinitrc.d/80-oscilloscope.sh
+# sudo /etc/X11/xinit/xinitrc.d/80-oscilloscope.sh
+##
+## else no xorg found, running on tty?
+sudo systemctl enable oscillo
 
-sudo /etc/X11/xinit/xinitrc.d/80-oscilloscope.sh
